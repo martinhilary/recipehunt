50a25df0320e99300eebcdf7eb6d7aa6a066ec84
martinhilary <martinhilary96@gmail.com>
2019-08-13 19:36:05 +0800

    Updates

---
+++ precache-manifest.eb769a0e004e45ba267e08e6eaca7ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c909b384973d83298a88ad218850c25",
+    "url": "/recipehunt/index.html"
+  },
+  {
+    "revision": "db0997e65fa0db59309a",
+    "url": "/recipehunt/static/css/main.23a5797f.chunk.css"
+  },
+  {
+    "revision": "573f771124ebfdec98dc",
+    "url": "/recipehunt/static/js/2.35493de6.chunk.js"
+  },
+  {
+    "revision": "db0997e65fa0db59309a",
+    "url": "/recipehunt/static/js/main.bbd53194.chunk.js"
+  },
+  {
+    "revision": "884f35d21ad74026b303",
+    "url": "/recipehunt/static/js/runtime~main.843b1e3f.js"
+  }
+]);
